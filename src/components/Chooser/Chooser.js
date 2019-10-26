b3fb59f018780d110aea3381b29485434d5d9048
Kevin Simpson <relasine@gmail.com>
2019-10-25 21:07:28 -0600

    Add Chooser and basic DD

--- src/components/Chooser/Chooser.js
+++ src/components/Chooser/Chooser.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "./Chooser.scss";
+import Dropdown from "../Dropdown/Dropdown";
+import PropTypes from "prop-types";
 
 class Chooser extends Component {
   constructor() {
@@ -7,14 +9,50 @@ class Chooser extends Component {
 
     this.state = {
       location: null,
-      food: null,
-      beer: null
+      foodSelection: null,
+      beerSelection: null,
+      deployed: null,
+      error: false
     };
   }
 
+  handleSelect = (name, selection) => {
+    this.setState({ [name]: selection, deployed: null });
+  };
+
+  handleDeploy = dropdown => {
+    if (this.state.deployed === dropdown) {
+      this.setState({ deployed: null });
+    } else {
+      this.setState({ deployed: dropdown });
+    }
+  };
+
+  handleSubmit = () => {
+    const { foodSelection, beerSelection, location } = this.state;
+    if (!foodSelection || !beerSelection || !location) {
+      this.setState({ error: true });
+      return;
+    }
+
+    this.props.handleSubmit(foodSelection, beerSelection, location);
+  };
+
   render() {
-    return <main className="bag-chooser"></main>;
+    const { beerOptions, foodOptions } = this.props;
+    return (
+      <main className="bag-chooser">
+        {beerOptions && <Dropdown options={beerOptions} name="beerSelection" />}
+        {foodOptions && <Dropdown options={foodOptions} name="foodSelection" />}
+      </main>
+    );
   }
 }
 
 export default Chooser;
+
+Chooser.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  beerOptions: PropTypes.arrayOf(PropTypes.string),
+  foodOptions: PropTypes.arrayOf(PropTypes.string)
+};
